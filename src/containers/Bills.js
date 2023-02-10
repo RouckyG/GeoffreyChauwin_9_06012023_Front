02d002cfeb511f76d9f18c8578030f5878bb0fdc
Roucky <geoffrey.chauwin@etudiants.univ-reims.fr>
2023-02-10 09:10:30 +0100

    fix Bills.js sort date

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -34,7 +34,6 @@ export default class {
       .list()
       .then(snapshot => {
         const bills = snapshot
-          .sort((a,b)=>{return (a.date < b.date) ? 1 : -1 })
           .map(doc => {
             try {
               return {
